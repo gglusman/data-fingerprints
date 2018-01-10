f354af4ee92a50c1a1abce8fd49061acb3887792
Gustavo <gglusman@Spice.local>
2018-01-10 09:49:49 -0800

    added documentation to LPH_multiple_XML.pl

--- bin/LPH_multiple_XML.pl
+++ bin/LPH_multiple_XML.pl
@@ -2,6 +2,27 @@
 $|=1;
 use strict;
 
+####
+#
+# This software library computes data fingerprints.
+# 
+# Copyright 2017 by Gustavo Glusman, Institute for Systems Biology, Seattle, WA, USA.
+# It is provided by the Institute for Systems Biology as open source software.
+# See the accompanying LICENSE file for information about the governing license.
+#
+####
+#
+# The first parameter is the directory where the XML files are located.
+# The second parameter is the fingerprint size to be used.
+# The third parameter specifies whether fingerprints need to be normalized.
+#
+# The standard output consists of fingerprints of the XML objects in the directory, one per line.
+# The first column is the identifier (filename).
+# The second column is the number of 'statements' observed while computing the fingerprint.
+# The remaining columns are the fingerprint itself.
+#
+####
+
 use lib "/users/gglusman/proj/LPH";
 use LIBLPH;
 my $LPH = new LIBLPH;
