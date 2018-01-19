e7675f7865f34229e55400f25730d7ddc84ed6d9
Gustavo Glusman <gustavo@systemsbiology.org>
2018-01-19 09:38:22 -0800

    added tool for computing on single JSON with multiple objects

---
+++ bin/LPH_JSON.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+$|=1;
+use strict;
+
+####
+#
+# This software library computes data fingerprints.
+# 
+# Copyright 2017 by Gustavo Glusman, Institute for Systems Biology, Seattle, WA, USA.
+# It is provided by the Institute for Systems Biology as open source software.
+# See the accompanying LICENSE file for information about the governing license.
+#
+####
+#
+# The first parameter is the file with the JSON objects to be studied.
+# The second parameter is the field to be used as identifier.
+# The third parameter is the fingerprint size to be used.
+# The fourth parameter specifies whether fingerprints need to be normalized.
+#
+# The standard output consists of fingerprints of the JSON objects in the file, one per line.
+# The first column is the object identifier.
+# The second column is the number of 'statements' observed while computing the fingerprint.
+# The remaining columns are the fingerprint itself.
+#
+####
+
+use lib "/users/gglusman/proj/LPH";
+use LIBLPH;
+my $LPH = new LIBLPH;
+
+use JSON;
+my($scanfile, $idField, $L, $normalize) = @ARGV;
+$L ||= 50;
+$LPH->{'L'} = $L;
+my(%cache, %cacheCount);
+my $decimals = 3;
+
+if ($scanfile =~ /\.gz$/) {
+	open JF, "gunzip -c $scanfile.gz |";
+} else {
+	open JF, $scanfile;
+}
+my @jsonContent = <JF>;
+close JF;
+chomp(@jsonContent);
+my $jsonString = join("", @jsonContent);
+next unless $jsonString;
+my $content = decode_json($jsonString);
+
+foreach my $entry (@$content) {
+	my $id = $entry->{$idField};
+	die "No $idField field found\n" unless defined $id;
+	delete $entry->{$idField};
+	$LPH->resetFingerprint();
+	$LPH->recurseStructure($entry);
+	my $fp;
+	if ($normalize) {
+		$fp = $LPH->normalize();
+	} else {
+		$fp = $LPH->{'fp'};
+	}
+	print join("\t", $id, $LPH->{'statements'}, map {sprintf("%.${decimals}f", $fp->[$_])} (0..$L-1)), "\n";
+}
+
